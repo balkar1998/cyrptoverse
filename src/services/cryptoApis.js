9396eeb7e29b572ee07056cc10dfc70945cd572a
balkar1998 <balkar48812@gmail.com>
2023-08-14 22:20:49 +0530

    delete extra code

--- src/services/cryptoApis.js
+++ src/services/cryptoApis.js
@@ -26,24 +26,3 @@ export const cryptoApi = createApi({
 });
 
 export const{ useGetCryptosQuery, useGetCryptoDetailQuery, useGetCryptoHistoryQuery } = cryptoApi;
-
-
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       tiers: '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': '07bbb27456mshcb52d9c96938a9cp1f4525jsn60f67f2e53c2'
-//     }
-//   };
-  
-  
